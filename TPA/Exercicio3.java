da032a84b5ccbf4b01d3dd9e30ef41bdb6972161
GabrielVeras007 <142914196+GabrielVeras007@users.noreply.github.com>
2023-11-21 10:28:00 -0300

    Add files via upload

---
+++ TPA/Exercicio3.java
@@ -0,0 +1,20 @@
+
+public class Exercicio3 {
+
+	public static void main(String[] args) {
+		int i, j, c;
+		
+		for(i=1;i<=10;i++) {
+			System.out.println("");
+			System.out.println("Tabuada do "+i);
+			for(j=1;j<=10;j++) {
+				c = j * i;
+				System.out.println(i+" x "+j+" = "+c);
+			}
+			
+			
+		}
+		
+	}
+
+}
